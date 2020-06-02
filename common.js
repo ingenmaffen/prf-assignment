5cda5236c01c94dd14464ebcee029f3692993646
Rabi Róbert <ingenmaffen@gmail.com>
2020-06-02 16:25:38 +0200

    Fix db url

--- common.js
+++ common.js
@@ -1,4 +1,5 @@
 module.exports = {
-  dbUrl: "mongodb://ingenmaffen:WhYwMxfa66646@ds217002.mlab.com:17002/",
+  dbUrl:
+    "mongodb://ingenmaffen:WhYwMxfa66646@ds217002.mlab.com:17002/heroku_rldtr627",
   dbName: "heroku_rldtr627",
 };
